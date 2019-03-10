786ceaf1a209316f38832c8ab43abfe713dc4d1f
Oleksandr Boiko <mega.boich@gmail.com>
2019-03-10 15:08:18 +0100

    Another implementation for DI, minification friendly

--- src/utils/di.ts
+++ src/utils/di.ts
@@ -5,16 +5,24 @@ export interface Abstract<T> {
   name: string;
 }
 
-let bindings: DictionaryLike<any> = {};
+interface Binding {
+  key: any;
+  value: any;
+}
+
+let bindings: Binding[] = [];
 
 export function resetBindings() {
-  bindings = {};
+  bindings = [];
 }
 
 export function register<T>(serviceIdentifier: Abstract<T>, instance: T): void {
-  const name = serviceIdentifier.name;
-  console.log("registering " + name);
-  bindings[name] = instance;
+  const recIndex = bindings.findIndex(b => b.key === serviceIdentifier);
+  if (recIndex >= 0) {
+    bindings[recIndex].value = instance;
+  } else {
+    bindings.push({ key: serviceIdentifier, value: instance });
+  }
 }
 
 export function registerMockService<T>(serviceIdentifier: Abstract<T>, mockInstance: Partial<T>) {
@@ -22,11 +30,9 @@ export function registerMockService<T>(serviceIdentifier: Abstract<T>, mockInsta
 }
 
 export function resolve<T>(serviceIdentifier: Abstract<T>): T {
-  const name = serviceIdentifier.name;
-  console.log("resolve " + name);
-  const instance = bindings[name];
-  if (!instance) {
-    throw new Error("Missed registration for " + name);
+  const rec = bindings.find(b => b.key === serviceIdentifier);
+  if (!rec) {
+    throw new Error("Missed registration for service: " + serviceIdentifier.name);
   }
-  return instance;
+  return rec.value;
 }
