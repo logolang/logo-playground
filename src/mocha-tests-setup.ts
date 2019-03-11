eb4c3a992c5d20d5b519a565f521be9742b68774
Oleksandr Boiko <mega.boich@gmail.com>
2019-03-11 20:02:39 +0100

    Fix unit tests

--- src/mocha-tests-setup.ts
+++ src/mocha-tests-setup.ts
@@ -4,4 +4,6 @@ import * as Adapter from "enzyme-adapter-react-16";
 // setup enzyme
 configure({ adapter: new Adapter() });
 
+(global as any).themesManagerGlobalInstance = {};
+
 console.log("Unit tests setup is done");
